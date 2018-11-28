273946db3dea2ac64dc15404ba0dd3ec03bd8a70
rcustodiog1300 <rcustodiog1300@alumno.ipn.mx>
2018-11-27 21:50:37 -0600

    Firebase Config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBEFo18XEygMRts7_JHBFmpDVBpKn_OpIc",
+    authDomain: "loginapp-85406.firebaseapp.com",
+    databaseURL: "https://loginapp-85406.firebaseio.com",
+    projectId: "loginapp-85406",
+    storageBucket: "loginapp-85406.appspot.com",
+    messagingSenderId: "554285176031"
+  }
 };
 
 /*
